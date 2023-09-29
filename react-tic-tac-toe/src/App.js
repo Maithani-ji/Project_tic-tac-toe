01746ec7e6153f17bdb1c85a65b7fb94c785160e
Maithani-ji <maithaniji.official@gmail.com>
2023-09-29 22:11:28 +0530

    first commit

---
+++ react-tic-tac-toe/src/App.js
@@ -0,0 +1,147 @@
+import logo from './logo.svg';
+import './App.css';
+
+import {useEffect, useState} from 'react';
+import {connect,Provider} from 'react-redux';
+import { createStore } from 'redux';
+
+const initial_state={
+  marks : [0,0,0,0,0,0,0,0,0],
+  player: 2,
+  gameover:false
+
+
+}
+const reducer=(state=initial_state,action)=>{
+  switch(action.type)
+  {
+  case 'SET_PLAYER':
+    return {...state,player:action.payload}
+    case  'SET_MARKS':
+      return {...state,marks:action.payload}
+      case 'SET_GAMEOVER':
+    return {...state,gameover:action.payload}
+      default:
+        return state;
+      }
+}
+const store=createStore(reducer);
+
+function App() {
+  return (
+    <div className="App">
+      <Provider store={store}>
+<Boardcontainer></Boardcontainer> 
+     </Provider>
+    
+    </div>
+  );
+}
+
+const mapStateToProps=(state)=>{
+return {
+  marks :state.marks,
+  player: state.player,
+  gameover:state.gameover
+}
+}
+ 
+const mapDispatchToProps=(dispatch)=>{
+return {
+  setMarks : (marks)=>{
+    dispatch({type:'SET_MARKS',payload:marks})
+  },
+  setPlayer:(player)=>{
+    dispatch({type:'SET_PLAYER',payload:player})
+
+  },
+  setGameover:(status)=>{
+    dispatch({type:"SET_GAMEOVER",payload:status})
+  }
+
+}
+}
+
+const Boardcontainer=connect(mapStateToProps,mapDispatchToProps)(Board);
+
+function Board({marks,setPlayer,player,setMarks,gameover,setGameover})
+{
+  // const [marks,setMarks]=useState([0,0,0,0,0,0,0,0,0]);
+  // const [player,setPlayer]=useState(1); // removing since using REDUX 
+
+  useEffect(()=>{
+    const comb=[
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6]
+    ]
+
+    for(let c of comb)
+    {
+      if(marks[c[0]]===1 && marks[c[1]]===1 && marks[c[2]]===1 )
+       {
+           console.log('player 1 wins');
+           setGameover(true);
+       }
+      if(marks[c[0]]===2 && marks[c[1]]===2 && marks[c[2]]===2)
+      {
+        console.log('player 2 wins');
+        setGameover(true);
+  
+      }
+    }
+  })
+
+
+  const changeMark= (i)=>{
+
+    const m= [...marks];
+    if(m[i]===0 && !gameover){
+    m[i]=player;
+    setMarks(m);
+    setPlayer(player===1 ?2:1);
+  }
+  else if(m[i]===0 && gameover)
+  {   
+    setPlayer(player===2 ?1:2);
+      alert(`Player ${player}wins`)
+  }
+  else{
+  alert('Please click on empty block')
+  }
+  }
+
+  return (
+    <div className="Board">
+<div>
+<Block mark={marks[0]} pos={0} changeMark={changeMark}></Block>
+<Block mark={marks[1]} pos={1} changeMark={changeMark}></Block>
+<Block mark={marks[2]} pos={2} changeMark={changeMark}></Block>
+</div>
+<div>
+<Block mark={marks[3]} pos={3} changeMark={changeMark}></Block>
+<Block mark={marks[4]} pos={4} changeMark={changeMark}></Block>
+<Block mark={marks[5]} pos={5} changeMark={changeMark}></Block>
+</div>
+<div>
+<Block mark={marks[6]} pos={6} changeMark={changeMark}></Block>
+<Block mark={marks[7]} pos={7} changeMark={changeMark}></Block>
+<Block mark={marks[8]} pos={8} changeMark={changeMark}></Block>
+</div>
+    </div>
+  )
+}
+
+function Block({mark,changeMark,pos})
+{
+  return <div className={`Block mark${mark}`}
+onClick={e=>changeMark(pos)}>
+    </div>
+  
+}
+export default App;
